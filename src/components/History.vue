753ab06806b85df843b6d36a3551705d1e7e0128
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2023-12-19 16:40:48 +0100

    first commit

---
+++ src/components/History.vue
@@ -0,0 +1,31 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li 
+            v-for="transaction in transactions" 
+            :class="transaction.amount > 0 ? 'plus' : 'minus'" 
+            :key="transaction.id"
+            >
+            {{transaction.text}} <span>{{ transaction.amount }}</span><button class="delete-btn" @click="deleteTransaction(transaction.id)">x</button>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+})
+
+const emit = defineEmits(['transactionDeleted']);
+
+
+const deleteTransaction = (id) => {
+    emit('transactionDeleted', id)
+}
+
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/style.css'
+
+import { createApp } from 'vue'
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+import App from './App.vue';
+
+const app = createApp(App);
+app.use(Toast);
+
+app.mount('#app')
